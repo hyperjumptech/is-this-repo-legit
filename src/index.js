11b37f4889d3f5f84b13ef06fd91296d18f88ccf
Nico Prananta <nico@hyperjump.tech>
2020-12-09 23:50:25 +0100

    use env variable for github token

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 
-const GITHUB_TOKEN = '<YOUR_GITHUB_TOKEN>';
-
 const client = new ApolloClient({
   link: new HttpLink({
     uri: `https://api.github.com/graphql`,
     headers: {
-      Authorization: `Bearer ${GITHUB_TOKEN}`
-    }
+      Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
   }),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
-
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
